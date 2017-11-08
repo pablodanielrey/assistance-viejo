b0f74e52146f91521de8db8b63fe3a2441063ed9
Iván <ivancas84@gmail.com>
2017-11-08 11:54:53 -0300

    versión inicial del de asistencia

---
+++ docker/ejecutar-ivan.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo docker run -ti -d --name assistance -v $(pwd)/src:/src -p 5040:5000 -p 5041:5001 -p 5042:5002 -p 5043:5003 --env-file $HOME/fce/fce/produccion/assistance assistance
+sudo docker exec -t assistance bash instalar.sh
+sudo docker restart assistance
